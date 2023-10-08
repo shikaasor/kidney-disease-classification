58924d5e852374a335fe5e8ff52b5dd63dfd5750
shikaasor <asor.tar23@gmail.com>
2023-10-08 17:36:39 +0100

    requirements added

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Kidney-Disease-Classification-Deep-Learning-Project"
-AUTHOR_USER_NAME = "krishnaik06"
+REPO_NAME = "kidney-disease-classification"
+AUTHOR_USER_NAME = "shikaasor"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "asor.tar23@gmail.com"
 
 
 setuptools.setup(
